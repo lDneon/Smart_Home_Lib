83e052f164e89a4ba5c5755fe75bd55c6faad160
lDneon <debreuslovemica@gmail.com>
2026-02-11 00:20:07 -0500

    library comp

---
+++ MiniSmartHomeLib/MiniSmartHomeLib/PowerModule.cs
@@ -0,0 +1,31 @@
+﻿namespace MiniSmartHomeLib
+{
+    public class PowerModule
+    {
+        public bool IsOnline { get; private set; }
+        public bool IsPoweredOn {get; private set;}
+
+
+        public void SetOline(bool online)
+                    {
+            IsOnline = online;
+        }
+
+        public void TurnOn ()
+        {
+               if (!IsOnline)
+                  throw new InvalidOperationException("Device must be online to power on.");
+
+
+        }
+
+        public void TurnOff()
+        {
+            if (!IsOnline)
+                IsPoweredOn = false;
+                
+        }
+
+
+    }
+}
---
+++ MiniSmartHomeLib/MiniSmartHomeLib/SmartDevice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniSmartHomeLib
+{
+    internal class SmartDevice
+    {
+        public string Device_Id { get; }
+        public string Name { get; private set; }
+        public PowerModule Power { get; }
+
+        public void Rename(string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Name cannot be blank.", nameof(newName));
+            Name = newName;
+
+        }
+
+        public virtual void SetOnline(bool online)
+        {
+            Power.SetOline(online);
+        }
+
+        public virtual void TurnOn()
+        {
+            Power.TurnOn();
+        }
+        public virtual void TurnOff()
+        {
+            Power.TurnOff();
+        }
+
+        public abstract string GetStatus();
+    }
+}
